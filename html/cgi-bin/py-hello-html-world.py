2b05f261aac04cc0705b4ea709147e19f5c213f4
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 08:33:55 -0700

    hello world py

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -1,25 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
-import datetime
-import socket
-
-print("Cache-Control: no-cache\n")
-print("Content-Type: text/x-python\n\n")
-print("<html>")
-print("<head>")
-print("<title>Hello, Python!</title>")
-print("</head>")
-print("<body>")
-
-print("<h1>Zhaoxing Lyu was here!  - Hello, Python!</h1>")
-print("<p>This page was generated with the Perl programming langauge</p>")
-
-date = datetime.datetime.now()
-print("<p>Current Time: ", date, " </p>")
-
-# # IP Address is an environment variable when using CGI
-# $address = $ENV{REMOTE_ADDR};
-#print ("<p>Your IP Address: ", socket.gethostbyname(socket.gethostname()), " </p>");
-
-print("</body>")
-print("</html>")
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('<title>Hello Word - First CGI Program</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello Word! This is my first CGI program</h2>')
+print ('</body>')
+print ('</html>')
