0fee4656fd7538da5056bc00a76d5e8aecdb5039
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 15:39:58 -0700

    check

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -4,8 +4,8 @@ import cgi
 import os
 
 
-#print ("Cache-Control: no-cache")
-print ("Content-type:text/html")
+print ("Cache-Control: no-cache")
+print ("Content-type:text/html\r\n\r\n")
 print ('<html>')
 print ('<head>')
 print ('<title>Hello, Python!</title>')
