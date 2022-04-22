5aee4a29d84e0861c1feb4f191f813cd699fc009
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 09:26:27 -0700

    test

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import datetime
+import socket
 
 #print ("Cache-Control: no-cache\n")
 print ("Content-type:text/html\r\n\r\n")
@@ -12,8 +13,8 @@ print ('<h1>Zhaoxing Lyu was here! -Hello, Python!</h1>')
 print ('<p>THis page was generated with the Python Programming language</p>')
 date = datetime.datetime.now().strftime("%c")
 print ('<p>Current Time: %s</p>' %(date))
-print ('<p>test</p>')
-#address = socket.gethostbyname(socket.gethostbyname())
-#print ('<p>Your IP Address: '+address+'</p>')
+#print ('<p>test</p>')
+address = socket.gethostbyname(socket.gethostbyname())
+print ('<p>Your IP Address: '+address+'</p>')
 print ('</body>')
 print ('</html>')
