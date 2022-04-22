da62abcbb5ec0f00f6f50fbe6a3fc0a6972ae4bd
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 08:57:22 -0700

    py hello world

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+import datetime
+import socket
 
+print("Cache-Control: no-cache\n")
 print ("Content-type:text/html\r\n\r\n")
 print ('<html>')
 print ('<head>')
-print ('<title>Hello Word - First CGI Program</title>')
+print ('<title>Hello, Python!</title>')
 print ('</head>')
 print ('<body>')
-print ('<h2>Hello Word! This is my first CGI program</h2>')
+print ('<h1>Zhaoxing Lyu was here! -Hello, Python!</h1>')
+print ('<p>THis page was generated with the Python Programming language</p>')
+date = datetime.datetime.now().strftime("%c")
+
+print ('<p>Current Time: %s', date)
 print ('</body>')
 print ('</html>')
