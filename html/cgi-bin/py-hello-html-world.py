2e0ca51de38a3c2b1fcfb93aecea3309cc4d24b5
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 08:12:59 -0700

    py hello world

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -5,7 +5,7 @@ import socket
 
 def main():
     print("Cache-Control: no-cache\n")
-    print("Content-type: text/html\n\n")
+    print("Content-Type: text/html\n\n")
     print("<html>")
     print("<head>")
     print("<title>Hello, Python!</title>")
