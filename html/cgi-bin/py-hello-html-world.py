c30f817b83cdecfe08365328428c72432ec1f8b1
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 09:21:16 -0700

    hello world py

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -2,6 +2,8 @@
 import datetime
 import socket
 
+from torch import add
+
 #print ("Cache-Control: no-cache\n")
 print ("Content-type:text/html\r\n\r\n")
 print ('<html>')
@@ -13,8 +15,7 @@ print ('<h1>Zhaoxing Lyu was here! -Hello, Python!</h1>')
 print ('<p>THis page was generated with the Python Programming language</p>')
 date = datetime.datetime.now().strftime("%c")
 print ('<p>Current Time: %s</p>' %(date))
-print ('<p>test</p>')
 address = socket.gethostbyname(socket.gethostbyname())
-print ('<p>Your IP Address: %s</p>' %(address))
+print ('<p>Your IP Address: '+address+'</p>')
 print ('</body>')
 print ('</html>')
