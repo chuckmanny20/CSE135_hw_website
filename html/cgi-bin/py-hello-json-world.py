d7e82c26cf4403c26c9aba6b15db7ddc6f8bfc6e
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 15:50:22 -0700

    py hello world json

--- html/cgi-bin/py-hello-json-world.py
+++ html/cgi-bin/py-hello-json-world.py
@@ -1,27 +1,23 @@
 import datetime
-import socket
 import json
-
-def main():
-    print("Cache-Control: no-cache\n")
-    print("Content-type: application/json\n\n")
-
-    date = datetime.datetime.now()
-    ads = socket.gethostbyname(socket.gethostname())
+import os
 
 
-    msg = {
-        "title": "Hello, Python!",
-        "heading": "Zhaoxing Lyu was here!  - Hello, Python!",
-        "message": "This page was generated with the Perl programming langauge",
-        "time": date,
-        "IP": ads
-    }
+print("Cache-Control: no-cache")
+print("Content-type: application/json\n\n")
 
-    j = json.dump(msg)
-    print(j)
+date = datetime.datetime.now()
+address = os.environ["REMOTE_ADDR"]
+
+msg = {
+    "title": "Hello, Python!",
+    "heading": "Zhaoxing Lyu was here!  - Hello, Python!",
+    "message": "This page was generated with the Perl programming langauge",
+    "time": date,
+    "IP": address
+}
+
+j = json.dump(msg)
+print(j)
 
 
-
-if __name__ == "__main__":
-    main()
