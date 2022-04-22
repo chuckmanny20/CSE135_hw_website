6bf6073c4d44faff71a3c66d88037c84d220bf1b
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 08:32:08 -0700

    py hello world

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -3,27 +3,23 @@
 import datetime
 import socket
 
-def main():
-    print("Cache-Control: no-cache\n")
-    print("Content-Type: text/x-python\n\n")
-    print("<html>")
-    print("<head>")
-    print("<title>Hello, Python!</title>")
-    print("</head>")
-    print("<body>")
+print("Cache-Control: no-cache\n")
+print("Content-Type: text/x-python\n\n")
+print("<html>")
+print("<head>")
+print("<title>Hello, Python!</title>")
+print("</head>")
+print("<body>")
 
-    print("<h1>Zhaoxing Lyu was here!  - Hello, Python!</h1>")
-    print("<p>This page was generated with the Perl programming langauge</p>")
+print("<h1>Zhaoxing Lyu was here!  - Hello, Python!</h1>")
+print("<p>This page was generated with the Perl programming langauge</p>")
 
-    date = datetime.datetime.now()
-    print("<p>Current Time: ", date, " </p>")
+date = datetime.datetime.now()
+print("<p>Current Time: ", date, " </p>")
 
-    # # IP Address is an environment variable when using CGI
-    # $address = $ENV{REMOTE_ADDR};
-    #print ("<p>Your IP Address: ", socket.gethostbyname(socket.gethostname()), " </p>");
+# # IP Address is an environment variable when using CGI
+# $address = $ENV{REMOTE_ADDR};
+#print ("<p>Your IP Address: ", socket.gethostbyname(socket.gethostname()), " </p>");
 
-    print("</body>")
-    print("</html>")
-
-if __name__ == "__main__":
-    main()
+print("</body>")
+print("</html>")
