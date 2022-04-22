c4e5eab66d6d9b336fc44b16a8c7983b4d9b7c1e
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 09:55:23 -0700

    check

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -15,6 +15,7 @@ print ('<h1>Zhaoxing Lyu was here! -Hello, Python!</h1>')
 print ('<p>THis page was generated with the Python Programming language</p>')
 date = datetime.datetime.now().strftime("%c")
 print ('<p>Current Time: %s</p>' %(date))
+print ('<p>test</p>')
 print (cgi.escape(os.environ["REMOTE_ADDR"]))
 #print ('<p>Your IP Address: %s</p>' %(address))
 
