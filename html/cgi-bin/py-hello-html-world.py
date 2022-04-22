bdf23949bda2be98486bb92764f0b402a0ebac99
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 09:33:57 -0700

    add address

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import datetime
-import socket
+import cgi
+import os
+
 
 #print ("Cache-Control: no-cache\n")
 print ("Content-type:text/html\r\n\r\n")
@@ -13,8 +15,7 @@ print ('<h1>Zhaoxing Lyu was here! -Hello, Python!</h1>')
 print ('<p>THis page was generated with the Python Programming language</p>')
 date = datetime.datetime.now().strftime("%c")
 print ('<p>Current Time: %s</p>' %(date))
-#print ('<p>test</p>')
-address = socket.gethostbyname(socket.gethostbyname())
-print ('<p>Your IP Address: '+address+'</p>')
+address = cgi.escape(os.environ["REMOTE_ADDR"])
+print ('<p>Your IP Address: %s</p>' %(address))
 print ('</body>')
 print ('</html>')
