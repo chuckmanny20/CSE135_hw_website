0d3436b56f2f66bb9f8bef2a3eec03a8c53fbf4a
2AM-official <zhaoxinglyu@icloud.com>
2022-04-22 09:04:39 -0700

    py hello world

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -13,6 +13,6 @@ print ('<h1>Zhaoxing Lyu was here! -Hello, Python!</h1>')
 print ('<p>THis page was generated with the Python Programming language</p>')
 date = datetime.datetime.now().strftime("%c")
 
-print ('<p>Current Time: %s', date)
+print ('<p>Current Time: %s' %(date))
 print ('</body>')
 print ('</html>')
