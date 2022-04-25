48b53185a3bfde61bc276a20980efc66aad2cd59
2AM-official <zhaoxinglyu@icloud.com>
2022-04-25 10:48:09 -0700

    print the query in get

--- html/cgi-bin/py-get-echo.py
+++ html/cgi-bin/py-get-echo.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+import os
+
+print("Cache-Control: no-cache")
+print("Content-type: text/html\n\n")
+print("<html><head><title>GET query string</title></head>\
+    </head><body><h1 align=center>GET query string</h1>\
+        <hr/> \n")
+
+# get and format query string
+print("Raw query string: %s\n<br/><br/>", os.environ.get("QUERY_STRING"))
+#print("<table> Formatted Query String:")
+query = os.environ.get("QUERY_STRING")
+print(query)
+
+
+
+#print("</table>")
+
+# Print HTML footer  
+print("</body>")
+print("</html>")
--- html/cgi-bin/py-post-echo.py
+++ html/cgi-bin/py-post-echo.py
@@ -1,12 +1,7 @@
-def main():
-    print("Cache-Control: no-cache\n")
-    print("Content-type: text/html\n\n")
-    print("<html><head><title>POST Request Echo</title></head>\
-        </head><body><h1 align=center>POST Request Echo</h1>\
-            <hr/> \n")
+#!/usr/bin/python3
 
-
-    print("</body></html>")
-
-if __name__ == "__main__":
-    main()
+print("Cache-Control: no-cache")
+print("Content-type: text/html\n\n")
+print("<html><head><title>POST Message Body</title></head>\
+    </head><body><h1 align=center>POST Message Body</h1>\
+        <hr/> \n")
