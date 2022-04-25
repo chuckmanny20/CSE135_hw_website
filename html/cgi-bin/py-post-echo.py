dad0cc2d40d82c7b22659bb9a672d56a9c515a91
2AM-official <zhaoxinglyu@icloud.com>
2022-04-25 12:34:46 -0700

    py post echo

--- html/cgi-bin/py-post-echo.py
+++ html/cgi-bin/py-post-echo.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+import sys
+
 print("Cache-Control: no-cache")
 print("Content-type: text/html\n\n")
 print("<html><head><title>POST Message Body</title></head>\
     </head><body><h1 align=center>POST Message Body</h1>\
         <hr/> \n")
+
+print("Message Body: %s\n<br/>", sys.stdin)
+
+# Print HTML footer
+print("</body>")
+print("</html>")
