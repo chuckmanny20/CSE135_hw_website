58f965d5785e4e27792627b6dd2e8ca84b7e9620
2AM-official <zhaoxinglyu@icloud.com>
2022-04-25 08:26:14 -0700

    py json hello world

--- html/cgi-bin/py-hello-json-world.py
+++ html/cgi-bin/py-hello-json-world.py
@@ -4,12 +4,14 @@ import json
 import os
 print("Cache-Control: no-cache")
 print("Content-type: application/json\n")
-date = datetime.datetime.now()
+date = datetime.datetime.now().strftime("%c")
 address = os.environ["REMOTE_ADDR"]
 msg = {
     'title': 'Hello, Python!',
     'heading': 'Zhaoxing Lyu was here!  - Hello, Python!',
-    'message': 'This page was generated with the Python programming langauge'
+    'message': 'This page was generated with the Python programming langauge',
+    'date': date,
+    'address': address
 }
 j = json.dumps(msg)
 print(j)
