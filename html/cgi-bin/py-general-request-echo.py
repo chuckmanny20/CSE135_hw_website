0b3871c861fe2ffb7b76965bf8a84f2e325e1712
2AM-official <zhaoxinglyu@icloud.com>
2022-04-25 12:00:16 -0700

    get env requesti method

--- html/cgi-bin/py-general-request-echo.py
+++ html/cgi-bin/py-general-request-echo.py
@@ -11,7 +11,7 @@ print("<html><head><title>General Request Echo</title></head>\
 # Get environment vars
 print("<table>\n")
 print("<tr><td>Protocol:</td><td>%s</td></tr>\n" %(os.environ.get("SERVER_PROTOCOL")))
-print("<tr><td>Method:</td><td>%s</td></tr>\n" %(os.environ.get("SERVER_PROTOCOL")))
+print("<tr><td>Method:</td><td>%s</td></tr>\n" %(os.environ.get("REQUEST_METHOD")))
 #print("<tr><td>Message Body:</td><td> %s</td></tr>\n", fgets(str, 1000, stdin))
 
 # Print HTML footer
