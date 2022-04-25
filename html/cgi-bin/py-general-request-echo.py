b62bf2bdfaa37cf8811edc9833643104f28ba79f
2AM-official <zhaoxinglyu@icloud.com>
2022-04-25 11:55:47 -0700

    py general requset

--- html/cgi-bin/py-general-request-echo.py
+++ html/cgi-bin/py-general-request-echo.py
@@ -1,5 +1,21 @@
-def main():
+#!/usr/bin/python3
+import os
+
+from matplotlib.pyplot import get
 
 
-if __name__ == "__main__":
-    main()
+print("Cache-Control: no-cache")
+print("Content-type: text/html\n\n")
+print("<html><head><title>General Request Echo</title></head>\
+    </head><body><h1 align=center>General Request Echo</h1>\
+        <hr/> \n")
+
+# Get environment vars
+print("<table>\n")
+print("<tr><td>Protocol:</td><td>%s</td></tr>\n" %(os.environ.get("SERVER_PROTOCOL")))
+print("<tr><td>Method:</td><td>%s</td></tr>\n" %(os.environ.get("SERVER_PROTOCOL")))
+#print("<tr><td>Message Body:</td><td> %s</td></tr>\n", fgets(str, 1000, stdin))
+
+# Print HTML footer
+print("</body>")
+print("</html>")
