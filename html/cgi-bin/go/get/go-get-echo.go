d192019b98daacc2bb5ed0ca73054c090c0cffaa
changliu020 <changliu020@gmail.com>
2022-04-27 16:05:49 -0700

    go get echo

--- html/cgi-bin/go/get/go-get-echo.go
+++ html/cgi-bin/go/get/go-get-echo.go
@@ -6,27 +6,22 @@ import (
 	"strings"
 )
 
-func main4() {
-	fmt.Print("Cache-Control: no-cache")
-	fmt.Print("Content-type: text/html\n\n")
-	fmt.Println("<html><head><title>Environment Variables</title></head>")
-	fmt.Println("<body><h1 align=center>Environment Variables</h1><hr/>")
-
-	for _, e := range os.Environ() {
-		pair := strings.SplitN(e, "=", 2)
-		fmt.Printf("<b>%20s</b>: %s\n<br/>", pair[0], pair[1])
-	}
-
-	fmt.Print("</body></html>")
-
+func main() {
 	fmt.Println("Cache-Control: no-cache")
 	fmt.Print("Content-type: text/html\n\n")
 	fmt.Println("<html><head><title>GET query string</title></head>")
 	fmt.Println("</head><body><h1 align=center>GET query string</h1><hr/>")
 
 	// TODO: QUERY_STRING
-	fmt.Println("Raw query string: \n<br/><br/>")
+	fmt.Printf("Raw query string: %s\n<br/><br/>\n", os.Getenv("QUERY_STRING"))
 	fmt.Println("<table> Formatted Query String:")
+	buffer := os.Getenv("QUERY_STRING")
+	for _, e := range strings.Split(buffer, "&") {
+		pair := strings.SplitN(e, "=", 2)
+		if len(pair) > 1 {
+			print("%s = %s <br/>\n", pair[0], pair[1])
+		}
+	}
 	fmt.Println("</table>")
 
 	fmt.Println("</body>")
