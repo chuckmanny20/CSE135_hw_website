403fdd7d3713a22dd08ef8c7c8fa787601730d14
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-27 08:18:34 -0700

    Get rid of html tags in PHP JSON

--- html/cgi-bin/php-hello-json-world.php
+++ html/cgi-bin/php-hello-json-world.php
@@ -3,24 +3,17 @@
     header("Content-type: text/json");
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello, PHP! (JSON)</title>
-    </head>
-    <body>
-        <?php
-            $message = "This page was generated with the PHP scripting langauge";
-            $date = date('l jS \of F Y h:i:s A');
-            $address = $_SERVER{REMOTE_ADDR};
+<?php
+    $message = "This page was generated with the PHP scripting langauge";
+    $date = date('l jS \of F Y h:i:s A');
+    $address = $_SERVER{REMOTE_ADDR};
 
-            $json_array = array("message"=>$message, "date"=>$date, "IP"=>$address);
+    $json_array = array("message"=>$message, "date"=>$date, "IP"=>$address);
         
-            $json_object = json_encode($json_array);
+    $json_object = json_encode($json_array);
         
-            print "<p>" . $json_object . "</p>";
-        ?>
-    </body>
-</html>
+    print "<p>" . $json_object . "</p>";
+?>
+
 
 
