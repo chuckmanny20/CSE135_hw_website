694377718caee44fd7b5d0c75a06f3b48409fd7d
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-27 00:23:21 -0700

    Test if Sessioning Page 1 works

---
+++ html/cgi-bin/php-sessions-1.php
@@ -0,0 +1,44 @@
+<?php
+    header("Cache-Control: no-cache");
+    header("Content-type: text/html");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Sessioning Page 1</title>
+    </head>
+    <body>
+        <h1>C Sessions Page 1</h1>
+        <?php
+            session_name("ZhaoXingID");
+            session_start();
+
+            $user = $_SESSION['username']
+
+            // Check to see if a proper name was sent
+            if ($user[0] == 'u')
+            {
+                name = username + 9;
+            }
+
+            print("<table>");
+
+            // First check for new Cookie, then Check for old Cookie
+            if (strlen($user) > 0)
+            {
+                print "<tr><td>Cookie:</td><td>%s</td></tr>\n" . $user;
+            }
+            else if ($_SESSION["HTTP_COOKIE"] != NULL && strcmp($_SESSION["HTTP_COOKIE"], "destroyed"))
+            {
+                printf "<tr><td>Cookie:</td><td>%s</td></tr>\n", $_SESSION["HTTP_COOKIE"];
+            }
+            else
+            {
+                print "<tr><td>Cookie:</td><td>None</td></tr>\n";
+            }
+
+            print "</table>";
+        ?>
+    </body>
+</html>
