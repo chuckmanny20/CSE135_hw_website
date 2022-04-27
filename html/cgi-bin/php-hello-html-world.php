28a3ed0c9569b97d50c4eee712a59288387969d7
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 18:41:38 -0700

    Beautify Hello World page

--- html/cgi-bin/php-hello-html-world.php
+++ html/cgi-bin/php-hello-html-world.php
@@ -10,12 +10,12 @@
     </head>
     <body>
         <?php
-            print "<h1>Liam was here - Hello, Perl!</h1>";
-            print "<p>This page was generated with the Perl programming langauge</p>";
+            print "<h1>Liam was here - Hello, PHP!</h1>";
+            print "<p>This page was generated with the PHP scripting langauge</p>";
             
             date_default_timezone_set('UTC');
 
-            print "<p>Current Time:" . date('l jS \of F Y h:i:s A') . "</p>";
+            print "<p>Current UTC Time:" . date('l jS \of F Y h:i:s A') . "</p>";
             
             # IP Address is an environment variable when using CGI
             $address = $_SERVER{REMOTE_ADDR};
