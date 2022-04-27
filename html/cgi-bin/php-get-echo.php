ad982d4724c505f70b08c94f254990e700aaa29f
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 22:14:23 -0700

    Found parse_str

--- html/cgi-bin/php-get-echo.php
+++ html/cgi-bin/php-get-echo.php
@@ -13,34 +13,15 @@
         <hr>
         <?php
             # The Query String is simply an environment variable
-            print "<b>Query String:</b>" . $_SERVER['QUERY_STRING'] . "<br />\n";
+            print "<b>Query String:</b>" . $_SERVER["QUERY_STRING"] . "<br />\n";
 
-            $query = $_SERVER['QUERY_STRING'];
-            $length = strlen($query);
+            $query = $_SERVER["QUERY_STRING"];
 
-            $in = [];
-
-            # Credit for this code to parse the Query string:
-            # https://www.mediacollege.com/internet/perl/query-string.html
-            if ( $length > 0) {
-                $buffer = $_SERVER['QUERY_STRING'];
-                $array = explode("&", $buffer);
-                foreach ($pair as $array) {
-                    $pieces = explode("=", $pair);
-                    $name = $pieces[0];
-                    $value = $pieces[1];
-
-                    $in += array($name => $value);
-                }
-            }
+            parse_str($query, $output);
 
             #Print out the Query String
-            $loop = 0;
-            foreach ($in as $name => $value) {
-                $loop += 1;
-                if($loop % 2 != 0) {
-                    print "$key = $in{$key}<br/>\n";
-                }
+            foreach ($output as $key => $value) {
+                print $key . " = " . $value . "<br/>\n";
             }
         ?>
     </body>
