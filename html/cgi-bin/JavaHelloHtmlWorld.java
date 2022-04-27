5b373119a0d99e637db4c2d5f3818bbf661c9a5a
changliu020 <changliu020@gmail.com>
2022-04-26 21:41:32 -0700

    java running

--- html/cgi-bin/JavaHelloHtmlWorld.java
+++ html/cgi-bin/JavaHelloHtmlWorld.java
@@ -5,18 +5,22 @@ public class JavaHelloHtmlWorld {
         // Calendar calendar = Calendar.getInstance();
 
         System.out.print("Cache-Control: no-cache\n");
-        System.out.print("Content-type: text/html\r\n\r\n");
+        System.out.print("Content-type:text/html\r\n\r\n");
 
-        System.out.println("<html><head>");
-        System.out.println("<title>Hello, Java!</title></head>" + 
-                        "<body><h1 align=center>Hello HTML World</h1>" +
-                        "<hr/>");
+
+        System.out.println("<html>");
+        System.out.println("<head>");
+        System.out.println("<title>Hello, Java!</title>");
+        System.out.println("</head>");
+        System.out.println("<body>");
+        System.out.println("<h1 align=center>Hello HTML World</h1>");
 
         // System.out.print("Chang Liu was here! - Hello, Java!<br/>\n");
         // System.out.print("This program was generated at: " + calendar.getTime() + "\n\n<br/>");
         // System.out.print("Your current IP address is: " + System.getenv("REMOTE_ADDR") + "<br/>");
 
         // Print HTML footer
-        System.out.print("</body></html>");
+        System.out.println("</body>");
+        System.out.println("</html>");
     }
 }
