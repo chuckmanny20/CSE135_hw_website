3e81c03859513743410fa671b6d69a185e256f5c
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 22:15:41 -0700

    Add comments

--- html/cgi-bin/php-get-echo.php
+++ html/cgi-bin/php-get-echo.php
@@ -17,9 +17,10 @@
 
             $query = $_SERVER["QUERY_STRING"];
 
+            # parses query string into $output as a Associative Array
             parse_str($query, $output);
 
-            #Print out the Query String
+            #Print out the Query String's key + value pairs
             foreach ($output as $key => $value) {
                 print $key . " = " . $value . "<br/>\n";
             }
