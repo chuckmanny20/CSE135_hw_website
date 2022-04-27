037f5110a3baf15de5327e2386686307436c9eb4
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 18:35:15 -0700

    Test PHP Hello World

---
+++ html/cgi-bin/php-hello-html-world.php
@@ -0,0 +1,25 @@
+<?php
+    header("Cache-Control: no-cache");
+    header("Content-type: text/html");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello, PHP!</title>
+    </head>
+    <body>
+        <?php
+            print "<h1>Liam was here - Hello, Perl!</h1>";
+            print "<p>This page was generated with the Perl programming langauge</p>";
+            
+            date_default_timezone_set('UTC');
+
+            print "<p>Current Time:", date('l jS \of F Y h:i:s A'), "</p>";
+            
+            # IP Address is an environment variable when using CGI
+            $address = $_SERVER{REMOTE_ADDR};
+            print "<p>Your IP Address: $address</p>";
+        ?>
+    </body>
+</html>
