1f19ec025f1395865cd9121c76dcfa7506e03266
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 19:05:15 -0700

    Use Array instead

--- html/cgi-bin/php-hello-json-world.php
+++ html/cgi-bin/php-hello-json-world.php
@@ -13,10 +13,12 @@
             $message = "This page was generated with the PHP scripting langauge";
             $date = date('l jS \of F Y h:i:s A');
             $address = $_SERVER{REMOTE_ADDR};
+
+            $json_array = array("message"=>$message, "date"=>$date, "IP"=>$address);
         
-            $json_object = json_encode(string $message, mixed $current_date, mixed $address);
+            $json_object = json_encode($json_array);
         
-            print "$json_object";
+            print "<p>" . $json_object . "</p>";
         ?>
     </body>
 </html>
