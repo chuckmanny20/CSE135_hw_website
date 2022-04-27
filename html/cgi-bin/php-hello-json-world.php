04547d59f7638d3ceb7e603ca138d9c8b7e2dfa3
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 18:55:11 -0700

    Add HTML to PHP Hello JSON

--- html/cgi-bin/php-hello-json-world.php
+++ html/cgi-bin/php-hello-json-world.php
@@ -1,13 +1,24 @@
 <?php
     header("Cache-Control: no-cache");
     header("Content-type: text/html");
-
-    $message = "This page was generated with the PHP scripting langauge"
-    $date = date('l jS \of F Y h:i:s A');
-    $address = $_SERVER{REMOTE_ADDR};
-
-    $json_object = json_encode(string $message, mixed $date, mixed $address);
-
-    print "$json_object";
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello, PHP!</title>
+    </head>
+    <body>
+        <?php
+            $message = "This page was generated with the PHP scripting langauge"
+            $date = date('l jS \of F Y h:i:s A');
+            $address = $_SERVER{REMOTE_ADDR};
+        
+            $json_object = json_encode(string $message, mixed $date, mixed $address);
+        
+            print "$json_object";
+        ?>
+    </body>
+</html>
+
 
