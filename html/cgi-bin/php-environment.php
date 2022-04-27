c2c528e7636688ff1a83acba7dcd424eda14c27c
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 19:43:44 -0700

    Try $_SERVER instead

--- html/cgi-bin/php-environment.php
+++ html/cgi-bin/php-environment.php
@@ -13,9 +13,10 @@
         <hr>
         <?php
             //getenv() without any arguments returns all of the 
-            $env_array = getenv();
+            //$env_array = getenv();
+            // $_ENV and getenv() is different from $_SERVER
 
-            foreach ($_ENV as $key=>$value) {
+            foreach ($_SERVER as $key=>$value) {
                 // nl2br replaces \n with HTML line breaks <br>
                 print nl2br("<b>" . $key . ":</b> " . $value . "\n");
             }
