0ed46a929eed68efa4d6143c8e488977ca2cb379
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 18:57:01 -0700

    Fix parse issue with $date

--- html/cgi-bin/php-hello-json-world.php
+++ html/cgi-bin/php-hello-json-world.php
@@ -11,10 +11,10 @@
     <body>
         <?php
             $message = "This page was generated with the PHP scripting langauge"
-            $date = date('l jS \of F Y h:i:s A');
+            $current_date = date('l jS \of F Y h:i:s A');
             $address = $_SERVER{REMOTE_ADDR};
         
-            $json_object = json_encode(string $message, mixed $date, mixed $address);
+            $json_object = json_encode(string $message, mixed $current_date, mixed $address);
         
             print "$json_object";
         ?>
