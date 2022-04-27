1786ab3919167a39e21bae442a4f70750ed517e1
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-04-26 18:58:18 -0700

    It wasn't $date it was a missing semicolon...

--- html/cgi-bin/php-hello-json-world.php
+++ html/cgi-bin/php-hello-json-world.php
@@ -10,8 +10,8 @@
     </head>
     <body>
         <?php
-            $message = "This page was generated with the PHP scripting langauge"
-            $current_date = date('l jS \of F Y h:i:s A');
+            $message = "This page was generated with the PHP scripting langauge";
+            $date = date('l jS \of F Y h:i:s A');
             $address = $_SERVER{REMOTE_ADDR};
         
             $json_object = json_encode(string $message, mixed $current_date, mixed $address);
