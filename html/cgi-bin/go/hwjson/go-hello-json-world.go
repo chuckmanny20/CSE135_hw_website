1c81728a976d9cebed6d0a6371b1030217da154e
changliu020 <changliu020@gmail.com>
2022-04-27 15:17:20 -0700

    go hw json

--- html/cgi-bin/go/hwjson/go-hello-json-world.go
+++ html/cgi-bin/go/hwjson/go-hello-json-world.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Println("Cache-Control: no-cache")
 	fmt.Println("Content-type: application/json")
-	fmt.Println("{\n\t\"message\": \"Hello World\",")
+	fmt.Println("\n{\n\t\"message\": \"Hello World\",")
 
 	fmt.Println("\t\"date\": \"" + time.Now().Format("2006-01-02 15:04:05") + "\",")
 
