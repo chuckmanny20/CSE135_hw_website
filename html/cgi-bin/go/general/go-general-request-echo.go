6bac46d3e7989d4fd0f7f579c362dc9970486322
changliu020 <changliu020@gmail.com>
2022-04-27 16:32:28 -0700

    go general

--- html/cgi-bin/go/general/go-general-request-echo.go
+++ html/cgi-bin/go/general/go-general-request-echo.go
@@ -6,27 +6,21 @@ import (
 	"bufio"
 )
 
-func main6() {
+func main() {
 	fmt.Println("Cache-Control: no-cache")
-	fmt.Println("Content-type: text/html")
-	fmt.Println("\n<html><head><title>General Request Echo</title></head>")
+	fmt.Print("Content-type: text/html\n\n")
+	fmt.Println("<html><head><title>General Request Echo</title></head>")
 	fmt.Println("</head><body><h1 align=center>General Request Echo</h1><hr/> ")
 
 	// Get environment vars
 	fmt.Println("<table>")
 	fmt.Printf("<tr><td>Protocol:</td><td>%s</td></tr>\n", os.Getenv(("SERVER_PROTOCOL")))
 	fmt.Printf("<tr><td>Method:</td><td>%s</td></tr>\n", os.Getenv(("REQUEST_METHOD")))
-	var inputReader *bufio.Reader
-	var input string
-	var err error
+	
+	inputReader := bufio.NewReader(os.Stdin)
+	input, _ := inputReader.ReadString('\n')
+	fmt.Printf("<tr><td>Message Body:</td><td> %s</td></tr>\n", input)
 
-	inputReader = bufio.NewReader(os.Stdin)
-	input, err = inputReader.ReadString('\n')
-    if err == nil {
-        fmt.Printf("<tr><td>Message Body:</td><td> %s</td></tr>\n", input)
-    }
-
-	// fmt.Println HTML footer
 	fmt.Println("</body>")
 	fmt.Println("</html>")
 
