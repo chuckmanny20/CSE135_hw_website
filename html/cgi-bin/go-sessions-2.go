6c3ed7bce84b0e6e7bf896f31bb626ae27bd57c8
changliu020 <changliu020@gmail.com>
2022-04-28 08:59:21 -0700

    go-session-2 minor modify

--- html/cgi-bin/go-sessions-2.go
+++ html/cgi-bin/go-sessions-2.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println("<head><title>Go Sessions</title></head>")
 	fmt.Println("<body>")
 	fmt.Println("<h1>Go Sessions Page 2</h1>")
-	// fmt.Println("<table>")
+	fmt.Println("<table>")
 	if os.Getenv("HTTP_COOKIE") != "" && os.Getenv("HTTP_COOKIE") != "destroyed" {
 		fmt.Printf("<tr><td>Cookie:</td><td>%s</td></tr>\n", os.Getenv("HTTP_COOKIE"))
 	} else {
@@ -31,7 +31,7 @@ func main() {
 	// Destroy Cookie button
 	fmt.Println("<form action=\"/cgi-bin/go-destroy-session.cgi\" method=\"get\">")
 	fmt.Println("<button type=\"submit\">Destroy Session</button>")
-	// fmt.Println("</form>")
+	fmt.Println("</form>")
 	fmt.Println("</body>")
 	fmt.Println("</html>")
 }
