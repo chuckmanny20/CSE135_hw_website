315d0de764711a3508b7a57ee50ad10df86527b0
changliu020 <changliu020@gmail.com>
2022-05-08 11:27:28 -0700

    part1 collector.js update

--- html/collector.js
+++ html/collector.js
@@ -1,25 +1,37 @@
 function staticCollect() {
-    const map = new Map();
-    alert(navigator.language);
-    // staticMap.set('userAgentStr', navigator.userAgent);
-    // staticMap.set('userLang', navigator.language);
-    // staticMap.set('userCookieAcceptance', navigator.cookieEnabled);
-    // // TODO: 3 manually figuring things
-    // staticMap.set('userScreenWidth', window.screen.width);
-    // staticMap.set('userScreenHeight', window.screen.height);
-    // staticMap.set('userWindowWidth', window.outerWidth);
-    // staticMap.set('userWindowHeight', window.outerHeight);
-    // console.log(map);
+    var staticMap = new Map();
+    staticMap.set('userAgentStr', navigator.userAgent);
+    staticMap.set('userLang', navigator.language);
+    staticMap.set('userCookieAcceptance', navigator.cookieEnabled);
+    // TODO: 3 manually figuring things
+    staticMap.set('userScreenWidth', window.screen.width);
+    staticMap.set('userScreenHeight', window.screen.height);
+    staticMap.set('userWindowWidth', window.outerWidth);
+    staticMap.set('userWindowHeight', window.outerHeight);
+    console.log(staticMap);
+}
+
+function performanceCollect() {
+    var performanceMap = new Map();
+    performanceMap.set('wholeTimingObject', window.PerformanceNavigationTiming);
+    var timeStartLoad = window.PerformanceNavigationTiming.startTime;
+    var timeEndLoad = window.PerformanceNavigationTiming.loadEventEnd;
+    performanceMap.set('timeStartLoad', timeStartLoad);
+    performanceMap.set('timeEndLoad', timeEndLoad);
+    performanceMap.set('timeTotalLoad', timeEndLoad - timeStartLoad);
+    console.log(performanceMap);
+}
+
+function activityCollect() {
+    document.onmousemove = function(event) {
+        pointerX = event.pageX;
+        pointerY = event.pageY;
+    }
+    function pointerCheck() {
+        console.log('Cursor at: ' + pointerX + ', ' + pointerY);
+    }
+    setInterval(pointerCheck, 1000);
 }
 
 staticCollect();
-
-// function performanceCollect() {
-//     const performanceMap = new Map();
-//     console.log(window.performance);
-// }
-
-// function activityCollect() {
-//     document.addEventListener();
-    
-// }
+performanceCollect();
