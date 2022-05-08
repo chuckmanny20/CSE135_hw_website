0543de36e376e7679fd8beed5ae903b5e1633203
changliu020 <changliu020@gmail.com>
2022-05-08 10:45:00 -0700

    part1 initialization

---
+++ html/collector.js
@@ -0,0 +1,23 @@
+function staticCollect() {
+    const map = new Map();
+    alert(navigator.language);
+    // staticMap.set('userAgentStr', navigator.userAgent);
+    // staticMap.set('userLang', navigator.language);
+    // staticMap.set('userCookieAcceptance', navigator.cookieEnabled);
+    // // TODO: 3 manually figuring things
+    // staticMap.set('userScreenWidth', window.screen.width);
+    // staticMap.set('userScreenHeight', window.screen.height);
+    // staticMap.set('userWindowWidth', window.outerWidth);
+    // staticMap.set('userWindowHeight', window.outerHeight);
+    // console.log(map);
+}
+
+// function performanceCollect() {
+//     const performanceMap = new Map();
+//     console.log(window.performance);
+// }
+
+// function activityCollect() {
+//     document.addEventListener();
+    
+// }
