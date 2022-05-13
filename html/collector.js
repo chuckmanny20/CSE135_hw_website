3837f9a5caa7634fb41633f37440009eaa493721
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-05-12 21:26:12 -0700

    Add localStorage and XHR to server

--- html/collector.js
+++ html/collector.js
@@ -8,6 +8,9 @@ function staticCollect() {
     staticMap.set('userScreenHeight', window.screen.height);
     staticMap.set('userWindowWidth', window.outerWidth);
     staticMap.set('userWindowHeight', window.outerHeight);
+
+    // localStorage
+    window.localStorage.setItem('staticCollection', staticMap);
 }
 
 function performanceCollect() {
@@ -27,6 +30,9 @@ function performanceCollect() {
     performanceMap.set('timeStartLoad', timeStartLoad);
     performanceMap.set('timeEndLoad', timeEndLoad);
     performanceMap.set('timeTotalLoad', timeEndLoad - timeStartLoad);
+
+    // localStorage
+    window.localStorage.setItem('performanceCollection', performanceMap);
 }
 
 function activityCollect() {
@@ -96,10 +102,85 @@ function activityCollect() {
         }
     });
     var curPage = doc.location.toString();
+
+    // localStorage
+    window.localStorage.setItem('idleEndList', idleEndList);
+    window.localStorage.setItem('idleLengthList', idleLengthList);
+
+    window.localStorage.setItem('cursorPosCollection', cursorPosMap);
+
+    window.localStorage.setItem('clickCollection', clickMap);
+
+    window.localStorage.setItem('scrollCollection', scrollMap);
+
+    window.localStorage.setItem('keyboardCollection', keyboardMap);
+
+    window.localStorage.setItem('visibleCollection', visibleMap);
+
+    window.localStorage.setItem('curPage', curPage);
+}
+
+function sendData() {
+
+    // grab all the items
+    staticCollection = window.localStorage.getItem('staticCollection');
+    performanceCollection = window.localStorage.getItem('performanceCollection');
+    idleEndList = window.localStorage.getItem('idleEndList');
+    idleLengthList = window.localStorage.getItem('idleLengthList');
+    cursorPosCollection = window.localStorage.getItem('cursorPosCollection');
+    clickCollection = window.localStorage.getItem('clickCollection');
+    scrollCollection = window.localStorage.getItem('scrollCollection');
+    keyboardCollection = window.localStorage.getItem('keyboardCollection');
+    visibleCollection = window.localStorage.getItem('visibleCollection');
+    curPage = window.localStorage.getItem('curPage');
+
+    // pack into XHR and send
+    // POST
+    let postXHR = new XMLHttpRequest();
+
+    // build JSON object
+    let JSONpacket = {}
+    JSONpacket['staticCollection'] = staticCollection
+    JSONpacket['performanceCollection'] = performanceCollection
+    JSONpacket['idleEndList'] = idleEndList
+    JSONpacket['idleLengthList'] = idleLengthList
+    JSONpacket['cursorPosCollection'] = cursorPosCollection
+    JSONpacket['clickCollection'] = clickCollection
+    JSONpacket['scrollCollection'] = scrollCollection
+    JSONpacket['keyboardCollection'] = keyboardCollection
+    JSONpacket['visibleCollection'] = visibleCollection
+    JSONpacket['curPage'] = curPage
+
+    // open up request
+    // will have to change the url for this later
+    // TODO: Split this into 3 XHRs for static, performance, and activity
+    postXHR.open('POST', 'https://zhaoxinglyu.site/api/posts', true);
+
+    // set Headers
+    // Want JSON back
+    // or postXHR.responseType = 'json';
+    postXHR.setRequestHeader('Content-Type', 'application/json');
+
+    // Add Event for when Response is fully loaded to show in output
+    // Have to put handleResponse call in anonymous function to get it to wait for readyState to actually be 4
+    postXHR.addEventListener('load', function() {
+        handleResponse(postXHR);
+    });
+
+    postXHR.send(JSON.stringify(JSONpacket));
+}
+
+function handleResponse(response) {
+    console.log('readyState:' + response.readyState)
+    console.log('status:' + response.status)
+    // puts 8 spaces for indenting JSON to make it nice and clean
+    packet = JSON.stringify(packet, null, 8);
+    console.log(packet)
 }
 
 staticCollect();
 performanceCollect();
 activityCollect();
 
-// store locally in a log file, and send periodcally
+// store locally in localStorage, and send periodcally (5 mins for now?)
+// XHR
