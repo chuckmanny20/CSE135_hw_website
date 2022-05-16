2f211b8668b4384bb068f4c08bd60f7de9f85bf7
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-05-16 01:56:35 -0700

    Add commas for list

--- html/collector.js
+++ html/collector.js
@@ -98,8 +98,8 @@ function activityCollect() {
             if( oldidleLengthList == 'null')
                 oldidleLengthList = '';
 
-            window.localStorage.setItem('idleEndList', oldidleEndList + idleEndList.toString());
-            window.localStorage.setItem('idleLengthList', oldidleLengthList + idleLengthList.toString());
+            window.localStorage.setItem('idleEndList', oldidleEndList + ',' + idleEndList.toString());
+            window.localStorage.setItem('idleLengthList', oldidleLengthList + ',' + idleLengthList.toString());
 
             // clear lists after adding
             idleEndList = [];
