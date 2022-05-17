bb711449635e464867bcddb1c39f4fa439d809f6
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-05-16 18:46:59 -0700

    Try to connect database to /static

--- html/server_api.js
+++ html/server_api.js
@@ -1,6 +1,14 @@
 // app.js file
 
-var jsonServer = require('/usr/local/lib/node_modules/json-server');
+const jsonServer = require('/usr/local/lib/node_modules/json-server');
+const mysql = require('mysql');
+const { connect } = require('http2');
+const connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : 'CSE135forSQL!',
+   database : 'CollectorDatabase'
+})
 
 // Returns an Express server
 var server = jsonServer.create();
@@ -9,7 +17,12 @@ var server = jsonServer.create();
 server.use(jsonServer.defaults());
 
 // Add custom routes
-// server.get('/custom', function (req, res) { res.json({ msg: 'hello' }) })
+server.get('/static', function (req, res) { 
+    connection.query("SELECT * from Static", (err, rows, fields) => {
+        console.log("I think we fetched");
+        res.json(rows);
+    });
+ })
 
 // Returns an Express router
 var router = jsonServer.router('db_api.json');
