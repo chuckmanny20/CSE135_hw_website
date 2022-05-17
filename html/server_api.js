20045a9f298a46ab8eb2023eaf93ba4748bd702d
chuckmanny20 <39514606+chuckmanny20@users.noreply.github.com>
2022-05-16 18:51:29 -0700

    Fix

--- html/server_api.js
+++ html/server_api.js
@@ -2,7 +2,6 @@
 
 const jsonServer = require('/usr/local/lib/node_modules/json-server');
 const mysql = require('mysql');
-const { connect } = require('http2');
 const connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
@@ -18,7 +17,7 @@ server.use(jsonServer.defaults());
 
 // Add custom routes
 server.get('/static', function (req, res) { 
-    connection.query("SELECT * from Static", (err, rows, fields) => {
+    connection.query("SELECT * from Static;", (err, rows, fields) => {
         console.log("I think we fetched");
         res.json(rows);
     });
