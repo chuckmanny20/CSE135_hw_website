c23b87e3c62d0bd76be7dae21b4c575ea4b8d142
changliu020 <changliu020@gmail.com>
2022-05-17 21:41:34 -0700

    GET request in getData.js

--- html/getData.js
+++ html/getData.js
@@ -1,10 +1,15 @@
-var httpRequest = new XMLHttpRequest();
-var url = 'https://zhaoxinglyu.site/api/static'
-httpRequest.open('GET', url, true);
-httpRequest.send();
-httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-        var json = httpRequest.responseText;
-        console.log(json);
-    }
-};
+// send GET request and get the JSON object return
+// dataType: static/performance/activity
+function GET_data(dataType) {
+    var httpRequest = new XMLHttpRequest();
+    var url = 'https://zhaoxinglyu.site/api/' + dataType;
+    httpRequest.open('GET', url, true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var json = httpRequest.responseText;
+            return json;
+        }
+    };
+}
+
