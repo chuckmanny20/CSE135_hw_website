571eb470fe6928557533a5884985764a77c9c5d6
changliu020 <changliu020@gmail.com>
2022-06-07 01:33:51 -0700

    view doesn't find error

--- reporting/public_html/server.js
+++ reporting/public_html/server.js
@@ -48,8 +48,8 @@ connection.query("SELECT * FROM userInfo;", (err, rows, fields) => {
   }
 });
 
-app.set('view-engine', 'ejs')
 app.set('views', __dirname + '/views');
+app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
